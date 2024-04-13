2453edac37d46fac28f69ded5a16dee072529407
Patrick Ineichen <github@frokonet.ch>
2024-04-13 11:11:15 +0200

    feat(examples): add GiftcardSample

---
+++ examples/GiftcardSample/Events.cs
@@ -0,0 +1,14 @@
+﻿namespace GiftcardSample;
+
+using SimpleDomain;
+
+public record GiftcardCreated(int CardNumber, decimal Balance, DateTime ValidUntil) : IEvent
+{
+    public string CardId => $"{this.CardNumber}";
+}
+
+public record GiftcardActivated(string CardId) : IEvent;
+
+public record GiftcardRedeemed(string CardId, decimal Amount) : IEvent;
+
+public record GiftcardLoaded(string CardId, decimal Amount) : IEvent;
