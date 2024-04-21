5c57a066b14beca5ebfa36caca0aa11cb70579f8
Patrick Ineichen <github@frokonet.ch>
2024-04-21 15:10:30 +0200

    feat(examples): use event sourced aggregate root test harness

--- examples/GiftcardSample.Facts/GiftcardTests.cs
+++ examples/GiftcardSample.Facts/GiftcardTests.cs
@@ -1,10 +1,14 @@
 ﻿namespace GiftcardSample;
 
-public class GiftcardTests : EventSourcedAggregateRootFixture<Giftcard>
+using SimpleDomain;
+
+public class GiftcardTests
 {
     private const int CardNumber = 12345;
     private static readonly string CardId = $"{CardNumber}";
     private static readonly DateTime ValidUntil = DateTime.Today.AddDays(1);
+
+    private readonly EventSourcedAggregateRootHarness<Giftcard> harness = new();
 
     [Fact]
     public void HasParameterlessConstructor()
@@ -16,141 +20,134 @@ public class GiftcardTests : EventSourcedAggregateRootFixture<Giftcard>
     [Fact]
     public void CreatesGiftcard()
     {
-        this.Create(() => Giftcard.Create(CardNumber, ValidUntil));
-        this.ShouldEmitEventLike<GiftcardCreated>(e =>
-            e is { CardNumber: CardNumber, Balance: 0m } && e.ValidUntil == ValidUntil && e.CardId == CardId);
+        this.harness.Create(() => Giftcard.Create(CardNumber, ValidUntil));
+        this.harness.ShouldEmitEventLike<GiftcardCreated>(e =>
+          e is { CardNumber: CardNumber, Balance: 0m } && e.ValidUntil == ValidUntil
+        );
     }
 
     [Fact]
     public void CreatesGiftcardWithInitialBalance()
     {
-        this.Create(() => Giftcard.Create(CardNumber, ValidUntil, 100m));
-        this.ShouldEmitEventLike(new GiftcardCreated(CardNumber, 100m, ValidUntil));
+        this.harness.Create(() => Giftcard.Create(CardNumber, ValidUntil, 100m));
+        this.harness.ShouldEmitEventLike(new GiftcardCreated(CardNumber, 100m, ValidUntil));
     }
 
     [Fact]
     public void ThrowsExceptionWhenTryingToCreateGiftcardWithNegativeNumber()
     {
-        this.Create(() => Giftcard.Create(-1, ValidUntil));
-        this.ShouldFailWith<ArgumentException>("Negative card number");
+        this.harness.Create(() => Giftcard.Create(-1, ValidUntil));
+        this.harness.ShouldFailWith<ArgumentException>("Negative card number");
     }
 
     [Fact]
     public void ThrowsExceptionWhenTryingToCreateAnExpiredGiftcard()
     {
-        this.Create(() => Giftcard.Create(CardNumber, DateTime.Today.AddDays(-1)));
-        this.ShouldFailWith<ArgumentException>("Already expired");
+        this.harness.Create(() => Giftcard.Create(CardNumber, DateTime.Today.AddDays(-1)));
+        this.harness.ShouldFailWith<ArgumentException>("Already expired");
     }
 
     [Fact]
     public void ThrowsExceptionWhenTryingToCreateGiftcardWithNegativeBalance()
     {
-        this.Create(() => Giftcard.Create(CardNumber, ValidUntil, -13m));
-        this.ShouldFailWith<ArgumentException>("Negative initial balance");
+        this.harness.Create(() => Giftcard.Create(CardNumber, ValidUntil, -13m));
+        this.harness.ShouldFailWith<ArgumentException>("Negative initial balance");
     }
 
     [Fact]
     public void ActivatesGiftcard()
     {
-        this.LoadFromHistory(new GiftcardCreated(CardNumber, 100m, ValidUntil));
-        this.Execute(g => g.Activate());
-        this.ShouldEmitEventLike(new GiftcardActivated(CardId));
+        this.harness.LoadFromHistory(new GiftcardCreated(CardNumber, 100m, ValidUntil));
+        this.harness.Execute(g => g.Activate());
+        this.harness.ShouldEmitEventLike(new GiftcardActivated(CardId));
     }
 
     [Fact]
     public void ThrowsExceptionWhenTryingToActivateAnAlreadyActivatedGiftcard()
     {
-        this.LoadFromHistory(
-            new GiftcardCreated(CardNumber, 100m, ValidUntil),
-            new GiftcardActivated(CardId));
-        this.Execute(g => g.Activate());
-        this.ShouldFailWith<InvalidOperationException>("Already active");
+        this.harness.LoadFromHistory(new GiftcardCreated(CardNumber, 100m, ValidUntil), new GiftcardActivated(CardId));
+        this.harness.Execute(g => g.Activate());
+        this.harness.ShouldFailWith<InvalidOperationException>("Already active");
     }
 
     [Fact]
     public void LoadsGiftcard()
     {
-        this.LoadFromHistory(
-            new GiftcardCreated(CardNumber, 100m, ValidUntil),
-            new GiftcardActivated(CardId));
-        this.Execute(g => g.Load(10m));
-        this.ShouldEmitEventLike(new GiftcardLoaded(CardId, 10m));
+        this.harness.LoadFromHistory(new GiftcardCreated(CardNumber, 100m, ValidUntil), new GiftcardActivated(CardId));
+        this.harness.Execute(g => g.Load(10m));
+        this.harness.ShouldEmitEventLike(new GiftcardLoaded(CardId, 10m));
     }
 
     [Fact]
     public void ThrowsExceptionWhenTryingToLoadGiftcardWithNegativeAmount()
     {
-        this.LoadFromHistory(
-            new GiftcardCreated(CardNumber, 100m, DateTime.Today.AddDays(-1)),
-            new GiftcardActivated(CardId));
-        this.Execute(g => g.Load(-10m));
-        this.ShouldFailWith<ArgumentException>("Negative amount");
+        this.harness.LoadFromHistory(
+          new GiftcardCreated(CardNumber, 100m, DateTime.Today.AddDays(-1)),
+          new GiftcardActivated(CardId)
+        );
+        this.harness.Execute(g => g.Load(-10m));
+        this.harness.ShouldFailWith<ArgumentException>("Negative amount");
     }
 
     [Fact]
     public void ThrowsExceptionWhenTryingToLoadANonActivatedGiftcard()
     {
-        this.LoadFromHistory(new GiftcardCreated(CardNumber, 100m, ValidUntil));
-        this.Execute(g => g.Load(10m));
-        this.ShouldFailWith<InvalidOperationException>("Not active");
+        this.harness.LoadFromHistory(new GiftcardCreated(CardNumber, 100m, ValidUntil));
+        this.harness.Execute(g => g.Load(10m));
+        this.harness.ShouldFailWith<InvalidOperationException>("Not active");
     }
 
     [Fact]
     public void ThrowsExceptionWhenTryingToLoadAnExpiredGiftcard()
     {
-        this.LoadFromHistory(
-            new GiftcardCreated(CardNumber, 100m, DateTime.Today.AddDays(-1)),
-            new GiftcardActivated(CardId));
-        this.Execute(g => g.Load(10m));
-        this.ShouldFailWith<InvalidOperationException>("Already expired");
+        this.harness.LoadFromHistory(
+          new GiftcardCreated(CardNumber, 100m, DateTime.Today.AddDays(-1)),
+          new GiftcardActivated(CardId)
+        );
+        this.harness.Execute(g => g.Load(10m));
+        this.harness.ShouldFailWith<InvalidOperationException>("Already expired");
     }
-
 
     [Fact]
     public void RedeemsGiftcard()
     {
-        this.LoadFromHistory(
-            new GiftcardCreated(CardNumber, 100m, ValidUntil),
-            new GiftcardActivated(CardId));
-        this.Execute(g => g.Redeem(10m));
-        this.ShouldEmitEventLike(new GiftcardRedeemed(CardId, 10m));
+        this.harness.LoadFromHistory(new GiftcardCreated(CardNumber, 100m, ValidUntil), new GiftcardActivated(CardId));
+        this.harness.Execute(g => g.Redeem(10m));
+        this.harness.ShouldEmitEventLike(new GiftcardRedeemed(CardId, 10m));
     }
 
     [Fact]
     public void ThrowsExceptionWhenTryingToRedeemGiftcardWithNegativeAmount()
     {
-        this.LoadFromHistory(
-            new GiftcardCreated(CardNumber, 100m, ValidUntil),
-            new GiftcardActivated(CardId));
-        this.Execute(g => g.Redeem(-10m));
-        this.ShouldFailWith<ArgumentException>("Negative amount");
+        this.harness.LoadFromHistory(new GiftcardCreated(CardNumber, 100m, ValidUntil), new GiftcardActivated(CardId));
+        this.harness.Execute(g => g.Redeem(-10m));
+        this.harness.ShouldFailWith<ArgumentException>("Negative amount");
     }
 
     [Fact]
     public void ThrowsExceptionWhenTryingToRedeemANonActivatedGiftcard()
     {
-        this.LoadFromHistory(new GiftcardCreated(CardNumber, 100m, ValidUntil));
-        this.Execute(g => g.Redeem(10m));
-        this.ShouldFailWith<InvalidOperationException>("Not active");
+        this.harness.LoadFromHistory(new GiftcardCreated(CardNumber, 100m, ValidUntil));
+        this.harness.Execute(g => g.Redeem(10m));
+        this.harness.ShouldFailWith<InvalidOperationException>("Not active");
     }
 
     [Fact]
     public void ThrowsExceptionWhenTryingToRedeemAnExpiredGiftcard()
     {
-        this.LoadFromHistory(
-            new GiftcardCreated(CardNumber, 100m, DateTime.Today.AddDays(-1)),
-            new GiftcardActivated(CardId));
-        this.Execute(g => g.Redeem(10m));
-        this.ShouldFailWith<InvalidOperationException>("Already expired");
+        this.harness.LoadFromHistory(
+          new GiftcardCreated(CardNumber, 100m, DateTime.Today.AddDays(-1)),
+          new GiftcardActivated(CardId)
+        );
+        this.harness.Execute(g => g.Redeem(10m));
+        this.harness.ShouldFailWith<InvalidOperationException>("Already expired");
     }
 
     [Fact]
     public void ThrowsExceptionWhenTryingToRedeemMoreThanTheBalance()
     {
-        this.LoadFromHistory(
-            new GiftcardCreated(CardNumber, 100m, ValidUntil),
-            new GiftcardActivated(CardId));
-        this.Execute(g => g.Redeem(110m));
-        this.ShouldFailWith<InvalidOperationException>("Insufficient balance");
+        this.harness.LoadFromHistory(new GiftcardCreated(CardNumber, 100m, ValidUntil), new GiftcardActivated(CardId));
+        this.harness.Execute(g => g.Redeem(110m));
+        this.harness.ShouldFailWith<InvalidOperationException>("Insufficient balance");
     }
 }
