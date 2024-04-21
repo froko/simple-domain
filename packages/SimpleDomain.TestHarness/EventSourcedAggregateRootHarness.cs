a6040b7d7ba9ec7937272bc2ba2577c7e11cb762
Patrick Ineichen <github@frokonet.ch>
2024-04-21 15:06:41 +0200

    feat: add test harness project

---
+++ packages/SimpleDomain.TestHarness/EventSourcedAggregateRootHarness.cs
@@ -0,0 +1,116 @@
+﻿namespace SimpleDomain;
+
+using FluentAssertions;
+
+/// <summary>
+/// The event source aggregate root harness
+/// </summary>
+/// <typeparam name="TAggregateRoot">The type of the aggregate root</typeparam>
+public class EventSourcedAggregateRootHarness<TAggregateRoot>
+    where TAggregateRoot : EventSourcedAggregateRoot
+{
+    private Func<TAggregateRoot> create = Activator.CreateInstance<TAggregateRoot>;
+    private Action<TAggregateRoot> execute = _ => { };
+    private EventHistory eventHistory = EventHistory.Create();
+
+    private TAggregateRoot aggregateRoot;
+    private Exception aggregateException = null!;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="EventSourcedAggregateRootHarness{TAggregateRoot}"/> class.
+    /// </summary>
+    public EventSourcedAggregateRootHarness() => this.aggregateRoot = this.create();
+
+    /// <summary>
+    /// Sets the aggregate root under test into the desired state
+    /// by replaying events from history
+    /// </summary>
+    /// <param name="events">The event history</param>
+    public void LoadFromHistory(params IEvent[] events) => this.eventHistory = new EventHistory(events);
+
+    /// <summary>
+    /// Creates a new aggregate root under test.
+    /// You only need to do this if you want to test a no parameterless constructor
+    /// </summary>
+    /// <param name="func">The function to create the aggregate root</param>
+    public void Create(Func<TAggregateRoot> func) => this.create = func;
+
+    /// <summary>
+    /// Executes behavior of the aggregate root under test
+    /// </summary>
+    /// <param name="action">The action to perform against the aggregate root</param>
+    public void Execute(Action<TAggregateRoot> action) => this.execute = action;
+
+    /// <summary>
+    /// Checks the occurrence of a specific exception
+    /// </summary>
+    /// <typeparam name="TException">The type of the exception</typeparam>
+    /// <param name="message">The expected exception message</param>
+    public void ShouldFailWith<TException>(string message)
+        where TException : Exception
+    {
+        this.Run();
+        this.aggregateException.Should().NotBeNull();
+        this.aggregateException.Should().BeOfType<TException>();
+        this.aggregateException.Message.Should().Be(message);
+    }
+
+    /// <summary>
+    /// Checks the occurrence of a specific event
+    /// </summary>
+    /// <typeparam name="TEvent">The type of the event</typeparam>
+    public void ShouldEmitEventLike<TEvent>(Func<TEvent, bool> expectation)
+        where TEvent : class, IEvent
+    {
+        this.Run();
+        this.aggregateRoot.UncommittedEvents.OfType<VersionableEvent>()
+            .Select(e => e.InnerEvent as TEvent)
+            .Should()
+            .ContainSingle(e => expectation(e!));
+    }
+
+    /// <summary>
+    /// Checks the occurrence of a specific event
+    /// </summary>
+    /// <typeparam name="TEvent">The type of the event</typeparam>
+    /// <param name="expectedEvent">An instance of the expected event in order to compare the properties</param>
+    public void ShouldEmitEventLike<TEvent>(TEvent expectedEvent)
+        where TEvent : class, IEvent
+    {
+        this.Run();
+        this.CheckForUncommittedEventContent(expectedEvent);
+    }
+
+    private void Run()
+    {
+        try
+        {
+            this.aggregateRoot = this.create();
+            this.aggregateRoot.LoadFromEventHistory(this.eventHistory);
+
+            this.execute(this.aggregateRoot);
+        }
+        catch (Exception exception)
+        {
+            this.aggregateException = exception;
+        }
+    }
+
+    private void CheckForUncommittedEventContent<TEvent>(TEvent expectedEvent)
+        where TEvent : class, IEvent
+    {
+        var actualEvent =
+            this.aggregateRoot.UncommittedEvents.OfType<VersionableEvent>().Select(e => e.InnerEvent).Last() as TEvent;
+
+        actualEvent
+            ?.Should()
+            .BeEquivalentTo(
+                expectedEvent,
+                options =>
+                    options
+                        .Using<DateTime>(
+                            ctx => ctx.Subject.Should().BeCloseTo(ctx.Expectation, TimeSpan.FromSeconds(1)))
+                        .WhenTypeIs<DateTime>()
+            );
+    }
+}
