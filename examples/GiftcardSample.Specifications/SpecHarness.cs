c7997911154006610441cb15a2992ea42de6770a
Patrick Ineichen <github@frokonet.ch>
2024-04-23 22:38:20 +0200

    feat(examples): improve GiftcardSample solution
    - Add MassTransit
    - Add command & event handlers
    - Add InMemoryReadStore
    - Add specification project with bbv.LamdaTales

---
+++ examples/GiftcardSample.Specifications/SpecHarness.cs
@@ -0,0 +1,34 @@
+﻿namespace GiftcardSample;
+
+using MassTransit;
+using MassTransit.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using SimpleDomain;
+using SimpleDomain.EventStore;
+
+public class SpecHarness(IServiceProvider provider)
+{
+    private readonly ITestHarness harness = provider.GetRequiredService<ITestHarness>();
+    private readonly IEventStore eventStore = provider.GetRequiredService<IEventStore>();
+
+    public Task StartUp() => this.harness.Start();
+
+    public Task WaitForMessageProcessing() => this.harness.InactivityTask;
+
+    public Task Send<T>(T message) where T : class => this.harness.Bus.Publish(message);
+
+    public async Task Replay(string aggregateId, params IEvent[] events)
+    {
+        var expectedVersion = events.Length - 1;
+        var version = 0;
+
+        var headers = new Dictionary<string, object>();
+        var versionableEvents = events
+            .Select(@event => new VersionableEvent(@event, version++, DateTimeOffset.Now))
+            .ToList();
+
+        using var eventStream = await this.eventStore.OpenStream<Giftcard>(aggregateId, CancellationToken.None);
+        await eventStream.Append(versionableEvents, expectedVersion, headers, CancellationToken.None);
+        await this.harness.Bus.PublishBatch((IEnumerable<object>)events);
+    }
+}
---
+++ examples/GiftcardSample/EventHandlerExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace GiftcardSample;
+
+using MassTransit;
+
+public static class EventHandlerExtensions
+{
+    public static void AddEventHandlers(this IBusRegistrationConfigurator configurator)
+    {
+        configurator.AddConsumer<GiftcardCreatedHandler>();
+        configurator.AddConsumer<GiftcardActivatedHandler>();
+        configurator.AddConsumer<GiftcardRedeemedHandler>();
+        configurator.AddConsumer<GiftcardLoadedHandler>();
+    }
+}
