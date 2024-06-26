e76738dbc2300a85436d1e5a18bb5776bfa8feae
Patrick Ineichen <github@frokonet.ch>
2024-06-26 22:07:23 +0200

    docs: add landing page w/ introduction

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -16,29 +16,8 @@ export default defineConfig({
       },
       sidebar: [
         {
-          label: 'Architecture',
-          items: [
-            { label: 'Introduction and Goals', link: '/arc42/introduction-and-goals' },
-            { label: 'Architecture Constraints', link: '/arc42/architecture-constraints' },
-            { label: 'System Scope and Context', link: '/arc42/system-scope-and-context' },
-            { label: 'Solution Strategy', link: '/arc42/solution-strategy' },
-            { label: 'Building Block View', link: '/arc42/building-block-view' },
-            { label: 'Runtime View', link: '/arc42/runtime-view' },
-            { label: 'Deployment View', link: '/arc42/deployment-view' },
-            { label: 'Cross-cutting Concepts', link: '/arc42/cross-cutting-concepts' },
-            { label: 'Architecture Decisions', link: '/arc42/architecture-decisions' },
-            { label: 'Quality Requirements', link: '/arc42/quality-requirements' },
-            { label: 'Risks and Technical Depts', link: '/arc42/risks-and-technical-depts' },
-            { label: 'Glossary', link: '/arc42/glossary' }
-          ]
-        },
-        {
-          label: 'Concepts',
-          autogenerate: { directory: 'concepts' }
-        },
-        {
-          label: 'Guides',
-          autogenerate: { directory: 'guides' }
+          label: 'Getting Started',
+          link: 'getting-started'
         },
         {
           label: 'References',
