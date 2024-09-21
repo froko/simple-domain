94a4795bf98bb9690bc839d964e042e20c51d129
Patrick Ineichen <github@frokonet.ch>
2024-09-21 19:09:18 +0200

    feat(examples): add Giftcard examples for NoBus & Rx

---
+++ examples/GiftcardSample.Rx.Specifications/SpecHarness.cs
@@ -0,0 +1,37 @@
+﻿namespace GiftcardSample;
+
+using Microsoft.Extensions.DependencyInjection;
+using SimpleDomain;
+using SimpleDomain.EventStore;
+
+public class SpecHarness
+{
+    private readonly Bus bus;
+    private readonly IEventStore eventStore;
+
+    public SpecHarness(IServiceProvider provider)
+    {
+        this.eventStore = provider.GetRequiredService<IEventStore>();
+        this.bus = provider.GetRequiredService<Bus>();
+        this.UseCases = provider.GetRequiredService<UseCases>();
+
+        provider.GetRequiredService<EventHandlers>().Subscribe();
+    }
+
+    public UseCases UseCases { get; }
+
+    public async Task Replay(string aggregateId, params object[] events)
+    {
+        var expectedVersion = events.Length - 1;
+        var version = 0;
+
+        var headers = new Dictionary<string, object>();
+        var versionableEvents = events
+            .Select(@event => new VersionableEvent(@event, version++, DateTimeOffset.Now))
+            .ToList();
+
+        using var eventStream = await this.eventStore.OpenStream<Giftcard>(aggregateId, CancellationToken.None);
+        await eventStream.Append(versionableEvents, expectedVersion, headers, CancellationToken.None);
+        await Task.WhenAll(events.Select(this.bus.Publish));
+    }
+}
---
+++ examples/GiftcardSample.Rx/Bus.cs
@@ -0,0 +1,18 @@
+﻿namespace GiftcardSample;
+
+using System.Reactive.Subjects;
+
+public sealed class Bus : IDisposable
+{
+    private readonly Subject<object> eventStream = new();
+
+    public IObservable<object> EventStream => this.eventStream;
+
+    public void Dispose() => this.eventStream.Dispose();
+
+    public Task Publish<TMessage>(TMessage message)
+    {
+        if (message is object @event) this.eventStream.OnNext(@event);
+        return Task.CompletedTask;
+    }
+}
