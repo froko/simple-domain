3e79d693cc9615afe3df46849cc167557f7f214a
Patrick Ineichen <github@frokonet.ch>
2024-10-04 23:17:38 +0200

    feat(examples): add MassTransit Giftcard example

---
+++ examples/GiftcardSample.MassTransit/CommandHandlers.cs
@@ -0,0 +1,50 @@
+﻿namespace GiftcardSample;
+
+using MassTransit;
+using SimpleDomain;
+
+internal sealed class CreateGiftcardHandler(IEventSourcedRepository repository) : IConsumer<CreateGiftcard>
+{
+    public Task Consume(ConsumeContext<CreateGiftcard> context)
+    {
+        var message = context.Message;
+        var giftcard = Giftcard.Create(
+            message.CardNumber,
+            message.ValidUntil,
+            message.Balance);
+        return repository.Save(giftcard, e => context.Publish(e));
+    }
+}
+
+internal sealed class ActivateGiftcardHandler(IEventSourcedRepository repository) : IConsumer<ActivateGiftcard>
+{
+    public async Task Consume(ConsumeContext<ActivateGiftcard> context)
+    {
+        var message = context.Message;
+        var giftcard = await repository.GetById<Giftcard>(message.CardId);
+        giftcard.Activate();
+        await repository.Save(giftcard, e => context.Publish(e));
+    }
+}
+
+internal sealed class RedeemGiftcardHandler(IEventSourcedRepository repository) : IConsumer<RedeemGiftcard>
+{
+    public async Task Consume(ConsumeContext<RedeemGiftcard> context)
+    {
+        var message = context.Message;
+        var giftcard = await repository.GetById<Giftcard>(message.CardId);
+        giftcard.Redeem(message.Amount);
+        await repository.Save(giftcard, e => context.Publish(e));
+    }
+}
+
+internal sealed class LoadGiftcardHandler(IEventSourcedRepository repository) : IConsumer<LoadGiftcard>
+{
+    public async Task Consume(ConsumeContext<LoadGiftcard> context)
+    {
+        var message = context.Message;
+        var giftcard = await repository.GetById<Giftcard>(message.CardId);
+        giftcard.Load(message.Amount);
+        await repository.Save(giftcard, e => context.Publish(e));
+    }
+}
